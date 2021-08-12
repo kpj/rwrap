a75c40992e16a5b875214c702b28ac1cdf531975
kpj <kim.philipp.jablonski@gmail.com>
2021-08-12 12:17:29 +0200

    Remove unnecessary logger modification

--- rwrap/__init__.py
+++ rwrap/__init__.py
@@ -1,18 +1,13 @@
 """Make using R packages from Python easier."""
 
-import sys
 from importlib import metadata
 
-from loguru import logger
 import rpy2.rinterface as ri
 
 from .wrapper import RLibraryWrapper
 
 
 __version__ = metadata.version("rwrap")
-
-logger.remove()
-logger.add(sys.stderr, level="WARNING")
 
 
 def __getattr__(name: str) -> RLibraryWrapper:
